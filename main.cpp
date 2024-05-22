0d8a2a22f7037976e89efbe0db882a6ec2c3c136
Yumee Chung <137648897+y0um33@users.noreply.github.com>
2024-05-22 13:07:48 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,117 +1,106 @@
-/*
-Proyecto Supermercado Main
-Yumee Chung
-A01712059
-
-Este es un proyecto para la clase TC1030 Programación Orientado a Objetos.
-Es un programa que muestra los productos disponibles con sus decuentos
-en el supermercado para los clientes. 
-Y para los staffs, permiten agregar productos en el supermercado.
-*/
-
 #include <iostream>
+
 #include "producto.h"
+
 #include "tienda.h"
 
 using namespace std;
 
-int main (int argc, char * argv[]){
-    
-    int opcion = 5;
-    int contrase = 0;
+int main(int argc, char * argv[]) {
+    int opcion = 11;
+    int constaff = 0;
+    int respuesta;
     string answer = "y";
-    int number = 8;
     string temp_nombre;
-    float temp_precio;
-    int temp_des;
-    int temp_ra;
+    double temp_precio;
+    double temp_desc;
 
     Tienda tienda;
 
-while (opcion != 0){
-    cout << "Bienvenidos a nuestro supermercado" << endl;
-    cout << "Eres cliente o staff?" << endl;
-    cout << "Si eres cliente escriba 1. \nSi eres staff escriba 2." << endl; //identificar el usuario
-    
-    if (opcion == 1){ //cliente
-        tienda.crea_productos();
-        cout << "Aquí está los productos disponibles con sus descuentos: " << endl;
-        tienda.muestra_producto();
-    }
+    while (opcion != 0){
+        cout << "BIENVENIDO A NUESTRO SUPERMERCADO" << endl;
+        cout << "1. Staff" << endl;
+        cout << "2. Cliente" << endl;
+        cout << "0. Salir" << endl;
+        cout << "Ingresa tu opción: ";
+        cin >> opcion;
 
-    if (opcion == 2)
-    {
-        cout << "Ingresa la contraseña del staff: " << ' ';
-        cin >> contrase;
-        if (contrase == 8956){
-            cout << "Bodega de Productos: " << endl;
-            tienda.muestra_producto();
-            cout << "Si quieres agregar productos inserta [SI] sino [NO]" << endl;
-            cin >> answer;
-            if (answer == "SI"){
-                cout << "Cual categoria quieres agregar producto?" << endl;
-                cout << "1. Fruta" << endl;
-                cout << "2. Juego" << endl;
-                cout << "3. Vegetal" << endl;
-                cout << "Inserta el inciso: " << ' ';
-                cin >> number;
-                if (number == 1)
-                {
-                    cout << "Nombre: " << endl;
+        if (opcion == 1){
+            cout <<"Ingresa la Contraseña del Staff:" << ' ';
+            cin >> constaff;
+            if (constaff == 8956) {
+                cout << "Los productos disponibles son: " << endl;
+                tienda.crea_productos();
+                tienda.muestra_productos();
+                cout << "Desea agregar productos? [SI] sino [NO]" << endl;
+                cin >> answer;
+                if (answer == "SI"){
+                    cout << "Que producto deseas agregar: " << endl;
+                    cout << "1. Fruta" << endl;
+                    cout << "2. Vegetal" << endl;
+                    cout << "3. Juego" << endl;
+                    cin >> respuesta;
+                    if (respuesta == 1)
+                    {
+                    cout << "Nombre del producto: " << ' ';
                     cin >> temp_nombre;
-                    cout << "Precio: " << endl;
+                    cout << "Precio del producto: " << ' ';
                     cin >> temp_precio;
-                    cout << "Descuentos: " << endl;
-                    cin >> temp_des;
-                    tienda.agrega_fruta(temp_nombre, temp_precio, temp_des);
-                    cout << "Lista de producto modificada: " << endl;
-                    tienda.muestra_producto();
-                }
-                else if (number == 2)
-                {
-                    cout << "Nombre: " << endl;
+                    cout << "Descuento del producto: " << ' ';
+                    cin >> temp_desc;
+                    tienda.agrega_fruta(temp_nombre,temp_precio,temp_desc);
+                    cout << "Producto modificado" << endl;
+                    tienda.muestra_productos();
+                    }//SI FRUTA
+
+                    else if (respuesta == 2)
+                    {
+                    cout << "Nombre del producto: " << ' ';
                     cin >> temp_nombre;
-                    cout << "Precio: " << endl;
+                    cout << "Precio del producto: " << ' ';
                     cin >> temp_precio;
-                    cout << "Descuentos: " << endl;
-                    cin >> temp_des;
-                    cout << "Rare (1 - 10): " << endl;
-                    cin >> temp_ra;
-                    tienda.agrega_juego(temp_nombre, temp_precio, temp_des, temp_ra);
-                    cout << "Lista de producto modificada: " << endl;
-                    tienda.muestra_producto();
-                }
-                else if (number == 3)
-                {
-                    cout << "Nombre: " << endl;
+                    cout << "Descuento del producto: " << ' ';
+                    cin >> temp_desc;
+                    tienda.agrega_vegetal(temp_nombre,temp_precio,temp_desc);
+                    cout << "Producto modificado" << endl;
+                    tienda.muestra_productos();
+                    }//SI VEGETAL
+
+                    else if (respuesta == 3)
+                    {
+                    cout << "Nombre del producto: " << ' ';
                     cin >> temp_nombre;
-                    cout << "Precio: " << endl;
+                    cout << "Precio del producto: " << ' ';
                     cin >> temp_precio;
-                    cout << "Descuentos: " << endl;
-                    cin >> temp_des;
-                    tienda.agrega_vegetal(temp_nombre, temp_precio, temp_des);
-                    cout << "Lista de producto modificada: " << endl;
-                    tienda.muestra_producto();
-                }
-                else {
-                    cout << "No existe ese valor en el inciso, Checa otra vez.";
-                    return 0;
-                }
-            }
-            else if (answer == "NO"){
-                cout << "Gracias, que tenga buen día";
-            }
-            else{
-                cout << "Error :(";
+                    cout << "Descuento del producto: " << ' ';
+                    cin >> temp_desc;
+                    tienda.agrega_juego(temp_nombre,temp_precio,temp_desc);
+                    cout << "Producto modificado" << endl;
+                    tienda.muestra_productos();
+                    }
+                    else {
+                        cout << "ERROR :(" << endl;
+                    }//else answer
+
+                } //SI
+
+                else if (answer == "NO"){
+                    cout << "Gracias" << endl;
+                }//NO
+
+            }//PIN STAFF
+
+            else {
+                cout << "NO TE PODEMOS IDENTIFICAR";
                 return 0;
-            }
-        } else {
-            cout << "CONTRASEÑA INCORRECTA, NO ERES STAFF";
-            return 0;
+            }//else pin
+
         }
 
-    } //cierre if staff
-
-} //cierre while
-
-} //cierre main
+        if (opcion == 2){
+            tienda.crea_productos();
+            cout << "Bienvenido a nuestro supermercado: " << endl;
+            tienda.muestra_productos();
+        } //usuario
+    }
+}; //fin main
